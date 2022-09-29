5a8ad71c9b7568be0b777a35664f17ea5277d2bd
Oze <insung1412@gmail.com>
2022-09-30 02:58:38 +0900

    Chapter_5(2)

--- C_Programing/operater_app.c
+++ C_Programing/operater_app.c
@@ -24,5 +24,54 @@ int main(void)
 
 	printf("%d\n", 3 == 2.5); // 3 -> double형으로 변환, 이형자료간 연산(-)가 일어나기 때문
 
+
+	// 합불합 분류
+
+	int pass = 0;
+
+	printf("키를 입력하세요 : ");
+	scanf_s("%d", &pass);
+	printf("결과 : %s\n", pass >= 150 ? "합격" : "불합격");
+
+
+	// 최댓값 구하기(토너먼트)
+
+	/*int nMax = 0;
+	int a, b, c;
+
+	scanf_s("%d %d %d", &a, &b, &c);
+	nMax = a > b ? a : b;
+	nMax = nMax > c ? nMax : c;
+
+	printf("%d", nMax);*/
+
+	
+	// 최댓값 구하기(서바이벌)
+
+	/*int nMax = -101, nInput = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		scanf_s("%d", &nInput);
+		nMax = nInput > nMax ? nInput : nMax;
+	}
+
+	printf("MAX : %d\n", nMax);*/
+
+
+	// 삼항 연산자 사용하는 이유
+
+	int nData = 0, nInput;
+	scanf_s("%d", &nInput);
+
+	if (nInput > 5)
+		nData = 10;
+	else
+		nData = 0;
+	printf("if문 : %d\n", nData);
+
+	nInput > 5 ? (nData = 10) : (nData = 0);
+	printf("삼항연산자 : %d", nData);
+
 	return 0;
 }
